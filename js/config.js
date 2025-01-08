663240c0a70378979c8eaf68d96c602398ecb1b5
tomer-regev <69435535+tomer-regev@users.noreply.github.com>
2025-01-08 14:51:44 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_A9IlyB6Uw', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3pvgje4ttio25p8v8s4ifbtq37', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
